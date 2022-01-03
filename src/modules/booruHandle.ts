bfc91f7cf360331341c94fabba3e81bf09f822a0
Phil <philip.schaefer0@gmail.com>
2022-01-03 04:54:58 +0100

    inital init

--- src/modules/booruHandle.ts
+++ src/modules/booruHandle.ts
@@ -1,5 +1,57 @@
 import axios from 'axios';
 
-import * as config from '../config.json';
+import config from '../config.json';
 
+type maturity = 'safe' | 'sketchy' | 'unsafe';
 
+const rawToken = `${process.env.szuruUser}:${process.env.szuruToken}`;
+
+const token = Buffer.from(rawToken).toString('base64');
+
+const api = axios.create({
+  baseURL: config.szuru.endpoint,
+  headers: {
+    authorization: `Token ${token}`,
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Host: config.szuru.host,
+  },
+  validateStatus(status) {
+    return status >= 200 && status < 500;
+  },
+});
+
+async function updateTag(tagName: string) {
+  // get tag version
+  const output = await api.get(`tag/${tagName}`);
+  // update tag
+  await api.put(`tag/${tagName}`, { version: output.data.version, category: 'artist' });
+}
+
+async function upload(contentUrl: string) {
+  const output = await api.post('uploads/', { contentUrl });
+  return output.data.token;
+}
+
+async function validate(contentToken: string) {
+  const output = await api.post('posts/reverse-search/', { contentToken });
+  return output.data.exactPost;
+}
+
+export async function createPost(contentUrl: string, tags: string[], artist: string, safety: maturity, desc?: string) {
+  // upload file
+  const contentToken = await upload(contentUrl);
+  if (await validate(contentToken)) return null;
+  // bundle tags
+  tags.push(artist);
+  // upload post
+  const postOut = await api.post('posts/', { tags, safety, contentToken });
+  // update special tags
+  await updateTag(artist);
+  // update desc
+  // TODO: not in scraper yet, DYI?
+  // if (desc) await api.post('comments/', { text: desc, postId: postOut.data.id });
+  return postOut.data;
+}
+
+export { createPost as default };
